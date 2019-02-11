e2550504b27d09d0627f284baf6e0b4ca71b2bb1
Oleg Port <oleg.portnikhin@eteron.ru>
2019-02-11 18:55:28 +0300

    postgres added

--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ import os
 PROJECTPATH = os.path.dirname(os.path.realpath(__file__))
 SQLITEPATH = os.path.join(PROJECTPATH, 'data.db')
 DBCONNECTION = 'sqlite:////{}'.format(SQLITEPATH)
+#DBCONNECTION = 'postgres://zkgjbiyo:0apXlSXRyZhDSxqcuYYpd97tOb220ho2@baasu.db.elephantsql.com:5432/zkgjbiyo'
